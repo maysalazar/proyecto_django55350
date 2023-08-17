a6f967beb47beca1f7ccad718c52fca321653f4a
Mairon salazar <maiornsalazar16@gmail.com>
2023-08-16 19:41:31 -0500

    botones de navegacion

--- control_estudios/views.py
+++ control_estudios/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from control_estudios.models import Curso, Estudiante
-
+from control_estudios.forms import CursoFormulario
 
 def listar_estudiantes(request):
     contexto = {
@@ -30,7 +30,8 @@ def listar_cursos(request):
     return http_response
 
 
-def crear_curso(request):
+def crear_curso_version_1(request):
+    
    if request.method == "POST":
        data = request.POST
        nombre = data['nombre']
@@ -43,7 +44,32 @@ def crear_curso(request):
    else:  # GET
        http_response = render(
            request=request,
-           template_name='control_estudios/formulario_curso_a_mano.html',
+           template_name='control_estudios/crear_curso_version_1.html',
        )
        return http_response
+   
+   
+def crear_curso(request):
+   if request.method == "POST":
+       #CREO UN OBEJTO FORMULARIO CON LA DATA QUE ENVIO EL USUARIO 
+       formulario = CursoFormulario(request.POST)
+
+       if formulario.is_valid():
+           data = formulario.cleaned_data  # es un diccionario
+           nombre = data["nombre"]
+           comision = data["comision"]
+           curso = Curso(nombre=nombre, comision=comision)  # lo crean solo en RAM
+           curso.save()  # Lo guardan en la Base de datos
+
+           # Redirecciono al usuario a la lista de cursos
+           url_exitosa = reverse('lista_cursos')  # estudios/cursos/
+           return redirect(url_exitosa)
+   else:  # GET
+       formulario = CursoFormulario()
+   http_response = render(
+       request=request,
+       template_name='control_estudios/formulario_curso.html',
+       context={'formulario': formulario}
+   )
+   return http_response
 
